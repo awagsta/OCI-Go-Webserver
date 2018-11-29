c69c13cec1907f13bcc467536dbc5c748f3f58c2
alexander wagstaff <awagstaff6@gmail.com>
2018-11-29 15:41:27 -0600

    Added Delete VCN. Will Refactor

--- handlers.go
+++ handlers.go
@@ -11,7 +11,7 @@ import (
 	"github.com/oracle/oci-go-sdk/core"
 )
 
-const compartmentID = "..."
+const compartmentID = "ocid1.compartment.oc1..aaaaaaaa6p62fu3x53o3lwv5p7apjotp7eyfrienrd3xbdy5py7s7pfjgssa"
 
 var config = common.DefaultConfigProvider()
 
@@ -117,4 +117,36 @@ func createVcn(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	writer.WriteHeader(http.StatusCreated)
 	json.NewEncoder(writer).Encode(v)
+}
+
+func deleteVCN(writer http.ResponseWriter, request *http.Request) {
+	var vcnID string
+	err := json.NewDecoder(request.Body).Decode(&vcnID)
+	if err != nil {
+		fmt.Println("Error decoding JSON request.")
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	c, err := core.NewVirtualNetworkClientWithConfigurationProvider(config)
+	if err != nil {
+		fmt.Println("Error. Could not authorize user.")
+		http.Error(writer, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	req := core.DeleteVcnRequest{VcnId: common.String(vcnID)}
+
+	_, err = c.DeleteVcn(context.Background(), req)
+	if err != nil {
+		fmt.Println("Delete VCN Failed")
+		http.Error(writer, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	result := "VCN with id " + vcnID + " Deleted Successfully."
+	fmt.Println("VCN Deleted Successfully")
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(result)
 }
